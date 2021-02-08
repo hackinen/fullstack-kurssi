1f34302e5b2b5740c97158ca4b6c5b50577dd98b
Iira <iira.hakkinen@helsinki.fi>
2021-02-08 17:29:51 +0200

    2.6

---
+++ osa2/puhelinluettelo/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+
+  const addPerson = (event) => {
+      event.preventDefault()
+      
+      const nameObject = {
+        name: newName,
+        id: newName
+      }
+
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+  }
+
+const handleNameChange = (event) => {
+    setNewName(event.target.value)
+}
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>name: <input
+            value={newName}
+            onChange={handleNameChange}    
+        /></div>
+        <div>
+        <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div>{person.name}</div>)}
+    </div>
+  )
+
+}
+
+export default App
