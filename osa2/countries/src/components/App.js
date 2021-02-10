f8d651cbd021496fe6058d7d008931a3af17e2cc
Iira <iira.hakkinen@helsinki.fi>
2021-02-10 10:32:28 +0200

    2.12

---
+++ osa2/countries/src/components/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [countries, setCountries] = useState([]) 
+  const [newFilterValue, setNewFilterValue] = useState('')
+
+  const filteredCountries = countries.filter(country => country.name.includes(newFilterValue))
+
+  useEffect(() => {
+    const eventHandler = response => {
+        console.log(response.data)
+      setCountries(response.data)
+    }
+  
+    const promise = axios.get('https://restcountries.eu/rest/v2/all')
+    promise.then(eventHandler)
+  }, [])
+
+
+  const handleFilterChange = (event) => {
+      setNewFilterValue(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Countries</h2>
+      <InputField text={'find countries: '} val={newFilterValue} handle={handleFilterChange} />
+      
+      <ListCountries countries={filteredCountries} />
+    </div>
+  )
+
+}
+
+const InputField = ({text, val, handle}) => {
+    return (
+        <div>
+            {text} <input 
+                value={val}  
+                onChange={handle}          
+            />
+        </div>
+    )
+}
+
+const ListCountries = ({countries}) => {
+    if (countries.length > 10) {
+        return (<div>Too many countries, specify another filter</div>)
+    } else if (countries.length === 1) {
+        return ( 
+            <div>
+                <ShowOneCountry country={countries[0]}/>
+            </div>
+        )
+    } else {
+        return ( 
+            <div>
+                {countries.map(country => <Country key={country.name} country={country} />)}
+            </div>
+        )
+    }
+    
+}
+
+const ShowOneCountry = ({country}) => {
+    return (
+        <div>
+            <h1>{country.name}</h1>
+                <div>capital {country.capital}</div>
+                <div>population {country.population}</div>
+            
+            <h2>languages</h2>
+                <ul>
+                    {country.languages.map(language => <li>{language.name}</li>)}
+                </ul>
+
+            <img 
+                src={country.flag}
+                alt="something went wrong"
+                width="200"    
+            />
+
+        </div>
+    )
+}
+
+const Country = ({country}) => {
+    return(
+        <div>{country.name}</div>
+    )
+  }
+
+
+export default App
