bfd0560291ac7c5ce3463b6b30594553f28af1d0
Iira <iira.hakkinen@helsinki.fi>
2021-03-04 15:07:55 +0200

    a-osan loput

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,14 +1,86 @@
+
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
 
 const totalLikes = (blogs) => {
-     return (blogs.map((blog) => blog.likes).reduce((sum, thisOne) => {
+    return (blogs.map((blog) => blog.likes).reduce((sum, thisOne) => {
         return sum + thisOne
     }, 0))
 }
 
+const favoriteBlog = (blogs) => {
+    const fave = blogs.reduce((mostLikes, blog) => {
+        if (blog.likes >= mostLikes.likes) {
+            return blog
+        } else {
+            return mostLikes
+        }
+    })
+
+    return (
+        {
+            title: fave.title,
+            author: fave.author,
+            likes: fave.likes
+        }
+    )
+}
+
+const mostBlogs = (blogs) => {
+    authors = lodash.countBy(blogs, (blog => blog.author))
+
+    let formatted = []
+
+    for (const [key,value] of Object.entries(authors)) {
+        formatted.push({
+            author: key,
+            blogs: value
+        })
+    }
+
+    return (
+        formatted.reduce((currentMost, blogger) => {
+            if (blogger.blogs >= currentMost.blogs) {
+                return blogger
+            } else {
+                return currentMost
+            }
+        })
+    )
+}
+
+const mostLikes = (blogs) => {
+    const authors = lodash.groupBy(blogs, (blog => blog.author))
+
+    let formatted = []
+
+    for (const [key,value] of Object.entries(authors)) {
+        const likes = value.map(blog => blog.likes).reduce((sum, current) => sum + current)
+
+        formatted.push({
+            author: key,
+            likes: likes
+        })
+    }
+
+    return (
+        formatted.reduce((currentMost, blogger) => {
+            if (blogger.likes >= currentMost.likes) {
+                return blogger
+            } else {
+                return currentMost
+            }
+        })
+    )
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
