5cd4f7f298b07f819ee19aff1f8dde9078d08be6
Iira <iira.hakkinen@helsinki.fi>
2021-03-22 15:46:12 +0200

    tehtävät 4.8-4.12

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,22 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
   
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    })
+    if (blog.title === undefined || blog.url === undefined) {
+      response.status(400).json()
+    } else {
+      blog.likes = blog.likes === undefined
+        ? 0
+        : blog.likes
+
+      const result = await blog.save()
+      response.status(201).json(result)
+    }
 })
 
 module.exports = blogsRouter
---
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,106 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const initialBlogs = [
+    {
+        title: 'blogi',
+        author: 'maija meikalainen',
+        url: 'www.blogi.fi',
+        likes: 4
+    },
+    {
+        title: 'toinen blogi',
+        author: 'matti meikalainen',
+        url: 'www.blogi2.fi',
+        likes: 1
+    },
+]
+
+const newBlog =
+    {
+        title: 'mun oma blogi',
+        author: 'bloggaaja',
+        url: 'www.jeemunblogi.fi',
+        likes: 60000
+    }
+
+const blogWithNoInitialLikes =
+    {
+        title: 'nollan tykkäyksen blogi',
+        author: 'bloggaaja',
+        url: 'www.givemelikes.fi',
+    }
+
+const blogWithNoTitle = 
+    {
+        author: 'matti',
+        url: 'www.eiotsikkoa.fi',
+        likes: 12
+    }
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+
+test('Blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+test('blog has a field named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+})
+
+test('when a new blog is added the number of blogs increases by one', async () => {
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(3)
+})
+
+test('the added blog has the right contents', async () => {
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body[2].title).toContain('mun oma blogi')
+})
+
+test('field like has a default value of 0', async () => {
+    await api
+        .post('/api/blogs')
+        .send(blogWithNoInitialLikes)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body[2].likes).toBe(0)
+})
+
+test('new blog has to contain both title and url', async () => {
+    await api
+        .post('/api/blogs')
+        .send(blogWithNoTitle)
+        .expect(400)
+})
+  
+afterAll(() => {
+    mongoose.connection.close()
+})
